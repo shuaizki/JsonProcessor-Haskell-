3269d45d0c3a5000febee67206bc630e8e9eaa6f
shuaizki <wshuaizki@gmail.com>
2015-01-06 00:26:13 +0800

    finish enclose
    just concat all components

--- json/Json.hs
+++ json/Json.hs
@@ -10,4 +10,3 @@ import Data.Char (toLower)
 data Value = StringValue String | NumberValue Double | BoolValue Bool | NullValue | ArrayValue [Value] | ObjectValue Object  deriving (Show, Read, Eq)
 
 data Object = EmptyObject | Objects [(String, Value)] deriving (Show, Read, Eq)
-
--- json/Prettify.hs
+++ json/Prettify.hs
@@ -12,9 +12,9 @@ data Doc = StringDoc String |
 
 escape_chars = [('\n', "\\n"), ('\b', "\\b"), ('\f', "\\f"), ('\r', "\\r"), ('\t', "\\t"), ('\\',"\\\\")]
 
-escape :: String -> Doc
+escape :: String -> String
 escape [] = ""
-escape (x:xs) = StringDoc ((processChar x (lookup x escape_chars)) ++ escape xs)
+escape (x:xs) = (processChar x (lookup x escape_chars)) ++ escape xs
         where processChar a Nothing = a:[]
               processChar a (Just c) = c
 
@@ -22,16 +22,27 @@ strToLower :: String -> String
 strToLower a = map toLower a
 
 renderValue :: Value -> Doc
-renderValue (NumberValue d) = StringDoc . show d
-renderValue (BoolValue b) = StringDoc . strToLower . show b
+renderValue (NumberValue d) = StringDoc $ show d
+renderValue (BoolValue b) = StringDoc $ strToLower $ show b
 renderValue (NullValue) = StringDoc "null"
-renderValue (StringValue a) = escape a
+renderValue (StringValue a) = StringDoc $ escape a
+renderValue (ArrayValue arr) = enclose '[' ']' renderValue arr
+renderValue (ObjectValue EmptyObject) = StringDoc "{}"
+renderValue (ObjectValue (Objects objs)) = enclose '{' '}' processKV objs
+        where processKV (key, value) = StringDoc key <> CharDoc ':' <> renderValue value
+
+enclose :: Char->Char->(a->Doc)->[a]->Doc
+enclose left right f value = CharDoc left <> mid <> CharDoc right
+                          where mid = foldr (<>) EmptyDoc . dconcat (CharDoc ',') . map f $ value
 
 (<>) :: Doc -> Doc -> Doc
 EmptyDoc <> x = x
 x <> EmptyDoc = x
 x <> y = Concat x y
 
---enclose with left and right
-enclose :: String->String->[Doc]->Doc
-enclose left right docs = foldr
+dconcat :: Doc->[Doc]->[Doc]
+dconcat _ [] = []
+dconcat d [x] = [x]
+dconcat d (x:xs) = (d <> x) : dconcat d xs
+
+--now pretty
